433d3eed04b703a3a168a462383bb3b473c25d26
Suramrit Singh <suramrit@buffalo.edu>
2018-12-17 23:43:07 -0800

    making some progress

--- hello.go
+++ hello.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
+//Some cool packages ..
+
+func main() { //entry point to the program
+	//Iodiomatic: Using patterns of speech in ways that they should be used...
 	fmt.Println("Howdy Y'all!")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println("Guess we are at", i)
+		}
+
+	}
+
+	foo()
+	//exit flow from the program
+}
+
+func foo() {
+	fmt.Println("Sup Matey!!")
 }
