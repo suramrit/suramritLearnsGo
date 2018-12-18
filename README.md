3857ecc2db62da7ac4060d81f2e63b20ebdc1fd3
Suramrit Singh <suramrit@buffalo.edu>
2018-12-18 10:21:53 -0800

    covered till TYPES and fmt package

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
 # suramritLearnsGo
 
+MIT License Copyright 2018 Suramrit
+
 Trying to get my hand dirty by doing some basic CS programs with Go. Lets see how it goes. 
 
-Feel free to use this code if you are a student, if you are a professional whos copying my code, I wish you all the best! 
+I'm starting with some basics like fmt package, TYPES, DECLARATIONS. 
+
+I will move onto doing some basic data structures implemented in golang including: 
+	array
+	linked lists - Singly / Doubly Linked 
+	FIFO Queue
+	LIFO Stack 
+	Binary Trees
+	Binary Heaps / Priority Queues
+	Hash Map/Sets and their types 
+
+Eventually I will also move to desigining and making some web services written in golang. 
+
+Feel free to use this code to learn if you are a student, complying with the rules and regulations of your school and class. 
+
+If you are a professional who is copying my code, dont know why you would do that but I wish you all the best! 
+
+Feel free to @ me: linkedin.com/in/suramrit 
 
 noteToSelf: stick to all lowercase for go instead of camelCase
--- hello.go
+++ hello.go
@@ -6,7 +6,7 @@ import (
 
 var y string     // Declare with Identifier and TYPE as string, assign ZERO value of string to "z"
 var z = "foobar" //entire program as scope .. limit global scope as best practice
-
+var l = 42       // declaring l as a type int..
 //Some cool packages ..
 func main() {
 	//entry point to the program
@@ -17,22 +17,34 @@ func main() {
 	x := 42 //both declares variable and assigns a value
 	fmt.Println(x)
 	x = x + 3
-	fmt.Println(x)
+	fmt.Printf("%T\n", x)
 	//y := 2 + x
 	y = "suramrit"
 	//var vs := > := cannot be outside a function body
 	fmt.Println(y)
-	fmt.Println(z)
-	for i := 0; i < 100; i++ {
-		if i%33 == 0 {
-			//Cant ignore the err return from the Println..
-			//Check the doc for return types.
-			n, _ := fmt.Println("Guess we are at", i, true, "didnt I say golang was cool!!")
-			fmt.Println(n)
-		}
+	fmt.Printf("%T\n", z)
+	// for i := 0; i < 100; i++ {
+	// 	if i%33 == 0 {
+	// 		//Cant ignore the err return from the Println..
+	// 		//Check the doc for return types.
+	// 		n, _ := fmt.Println("Guess we are at", i, true, "didnt I say golang was cool!!")
+	// 		fmt.Println(n)
+	// 	}
 
-	}
-	foo()
+	// }
+	//foo()
+	//Types in golang -- golang is all about type
+	//Type can be printed using %T in fmt.Printf()
+	x = 10051991
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)       //type
+	fmt.Printf("%b\n", x)       //binary
+	fmt.Printf("%x\n", x)       //HEX
+	fmt.Printf("%#x\n", x)      //0xHEX
+	s := fmt.Sprintf("%b\n", x) // Binary as String
+	fmt.Println(s + `suramrit   
+					singh`) // `` for literal string
+
 	//exit flow from the program
 }
 func foo() {
