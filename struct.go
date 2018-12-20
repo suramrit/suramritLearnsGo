8f88a3465f2cad4c836ac86fe1ad119e106d51a7
Suramrit Singh <suramrit@buffalo.edu>
2018-12-20 01:32:18 -0800

    added struct.go

---
+++ struct.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+type phrmagirl struct {
+	person   // donot give a field name
+	liscence int
+	location string
+}
+
+//Embedded Structs -- enterin OOP in golang
+// spec doc at --https://golang.org/ref/spec#Struct_types
+type engineer struct {
+	person
+	field  string
+	degree string
+}
+
+func main() {
+	//Struct -- aggregate data type
+	s := person{ //composite literal
+		first: "Suramrit",
+		last:  "Singh",
+		age:   1000,
+	}
+	j := person{
+		first: "jackie",
+		last:  "diane", // will assign zero value to the age
+	}
+	fmt.Println(s.last, j, j.age)
+
+	e1 := engineer{
+		person: s,
+		field:  "computer science",
+		degree: "masters",
+	}
+
+	p1 := phrmagirl{ // if all the fields are different, can do without specifying them
+		j,
+		318732,
+		"Leavenworth",
+	}
+	fmt.Println(e1.first, p1) //inner type gets promoted to the outer type.. can specify namespace in case of a collision
+	fmt.Println(p1.person)
+	//Anon Structs -- used to avoid code pollution
+	anon_struct := struct {
+		name string
+		age  int
+	}{
+		name: "suramrit",
+		age:  22,
+	}
+	fmt.Println(anon_struct.age)
+}
