c6ac0f3566c46c94ad17d18c65bf2442e594197d
Suramrit Singh <suramrit@buffalo.edu>
2018-12-22 17:45:23 -0800

    added functions.go

---
+++ functions.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+//Functions -- making thigns modular
+//packages
+//Everything in go is PASS BY VALUE!!
+
+func main() {
+	bar("yo wasssupp!")
+	z := returner()
+	fmt.Println(z)
+	x, y := multi("jakie", "diane")
+	fmt.Println(x)
+	fmt.Println(y)
+	sum(1, 2, 3, 45, 5, 6, 6, 7, 77)
+	s := []int{1, 2, 3, 45, 5, 6, 6, 7, 77}
+	sum()     //will run as sum() is variadic
+	sum(s...) //pass slice as a simple list
+}
+
+//creating function
+// func (r reciever) identifier(parameters) (returns(s)) { .. }
+
+func foo() { //parameter
+	fmt.Println("inside foo!! ")
+}
+
+func bar(s string) {
+	fmt.Println(s)
+}
+
+func returner() string {
+	defer foo() //deferred functions
+	return "woo!"
+}
+
+//multiple return
+
+func multi(fn, ln string) (string, bool) {
+	a := fmt.Sprint(fn, " ", ln, ` said "hello!"`)
+	b := true
+	return a, b
+}
+
+//variadic parameters in functions
+func sum(x ...int) {
+	//Type:: []int .. pass individual items, which are stored in the function stack as a slice
+	fmt.Println(x)
+	sum := 0
+	for _, v := range x {
+		sum += v
+	}
+	fmt.Println(sum)
+}
+
+//Modularising code
+
+//Decoupling -- using defer
+//deferred function is not run until the surrounding function executes and returns
