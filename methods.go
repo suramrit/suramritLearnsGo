8e95d122e9c94b7c14bcb0caca1919cbfc589e22
Suramrit Singh <suramrit@buffalo.edu>
2018-12-22 17:45:57 -0800

    added methods.go

---
+++ methods.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+//Methods -- attached to structs
+
+type person struct {
+	first string
+	last  string
+}
+
+type student struct {
+	person
+	univ   string
+	enroll int
+	loan   int
+}
+
+func (s *student) addloan(amt int) {
+	s.loan = amt
+}
+
+//func (r receiver)<!attaches the function to the value of the type> identifier(param) (returns(s))
+
+func main() {
+	s1 := student{
+		person: person{
+			first: "suramrit",
+			last:  "singh",
+		},
+		univ:   "U of Buffalo",
+		enroll: 232523,
+	}
+	fmt.Println(s1)
+	s1.addloan(5000)
+	fmt.Println(s1)
+}
