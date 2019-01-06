f4eaea9f39ac8c60a6491d33ed49ec358daced3e
Suramrit Singh <suramrit@buffalo.edu>
2019-01-06 08:37:18 +0530

    channels.go - better documentation for ranging over a channel

--- channels.go
+++ channels.go
@@ -4,20 +4,18 @@ import("fmt"
 )
 
 func main() {
-	fmt.Println("hey hargobind, hope you are having a nice day!")
-	fmt.Printf("Hi hargobind, I am told you are %d years old\n", 19)
 	c := make(chan int,3) //buffered channel 
 	//Channels Block! 
 	//c <- 32 //This will block.. 
 			//until send and recieve at the same time... 
 			//or  buffered channel 
 	go func() {
-		c<-332 // Write to channel .. routine is blocked until its read
+		c<-332 // Write to channel .. routine is blocked until channel is read
 	}()
-	fmt.Println(<-c) //reads from the channel... once read, goroutine can unblock
-	//fmt.Println(<-c)
+	fmt.Println(<-c) // reads from the channel... once read, goroutine can unblock
+	// fmt.Println(<-c)
 
-	//Directional Channels - only send to or recieve from channel
+	// Directional Channels - only send to or recieve from channel
 
 	fmt.Printf("%T\t\n", c)
 
@@ -25,40 +23,42 @@ func main() {
 	recv_only := make(<-chan int) // send only
 	fmt.Printf("%T\t\n", only_send_channel)
 	fmt.Printf("%T\t\n", recv_only)
-	//recv_only <- 32 //error! 
+	// recv_only <- 32 //error! 
 	go func () {
 		only_send_channel <- 223
 	}()
-	//fmt.Println(<- only_send_channel) //error!
+	// fmt.Println(<- only_send_channel) //error!
 
-	//Using send and recieve channels 
+	// Using send and recieve channels 
 
-	//send
+	// send
 	go sender(c)
 
-	//recieve
-	c<-39
-	//reciever(c)
+	// recieve
+	// ec<-39
+	// reciever(c)
 
-	//Ranging over a channel -- blocks until the channel is closed
-
+	// Ranging over a channel -- blocks until the channel is closed
+	
 	for v:= range c {
-		fmt.Println(v)
+		fmt.Println(v) // will result in deadlock until the channel is closed.
+		// To break such iteration channel needs to be closed explicitly. 
+		// Otherwise range would block forever in the same way as for nil channel
 	}
 
-	//Select Statement 
-	eve := make(chan int)
-	odd:= make (chan int)
-	quit:= make(chan int)
+	// Select Statement 
+	// eve := make(chan int)
+	// odd:= make (chan int)
+	// quit:= make(chan int)
 
-	//go send(eve, odd, quit)
+	// go send(eve, odd, quit)
 
-	//select_recieve(eve, odd, quit) 
+	// select_recieve(eve, odd, quit) 
 
 	// comma ok idiom in select statements 
-	//quit = make(chan bool)
-	go send(eve,odd,quit)
-	comma_ok_select(eve,odd,quit)
+	// quit = make(chan bool)
+	// go send(eve,odd,quit)
+	// comma_ok_select(eve,odd,quit)
 
 
 }
