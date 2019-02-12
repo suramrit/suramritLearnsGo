6f528c641f8e4807d1a7c03e650c31506fdfff24
Suramrit Singh <suramrit@buffalo.edu>
2019-02-11 22:43:57 -0800

    refactoring code

--- websiteRacer/racer.go
+++ websiteRacer/racer.go
@@ -1,23 +1,42 @@
 package main 
 
 import ("time";
-		"net/http")
-func race(u1, u2 string) string{
+		"net/http";
+		"errors")
 
+var httpGetTimeOut = errors.New("Time out while getting response!")
+const tenSecondTimeOut = time.Second * 10
 
-	aDuration := urltime(u1)
-	bDuration := urltime(u2)
+func race(u1,u2 string) (string,error) {
+	return customrace(u1,u2,tenSecondTimeOut)
 
-	if aDuration > bDuration {
-		return u2
-	}
-
-	return u1
 }
 
-func urltime(u string) time.Duration {
+
+func customrace(u1, u2 string, t time.Duration) (string, error) {
+		select {
+			case <-ping(u1) :
+				return u1,nil
+			case <-ping(u2) :
+				return u2,nil
+			case <-time.After(t):
+        		return "", httpGetTimeOut
+		}
+}
+
+/*func urltime(u string) time.Duration {
 	start := time.Now()
     http.Get(u)
     duration := time.Since(start)
     return duration
+}*/
+
+func ping(u string) (chan bool) {
+	ch := make (chan bool)
+
+	go func(){
+		http.Get(u)
+		ch <- true
+		}()
+	return ch
 }
--- websiteRacer/racer_test.go
+++ websiteRacer/racer_test.go
@@ -3,32 +3,53 @@ package main
 import ("testing";
 		"net/http/httptest";
 		"net/http";
-		"time")
+		"time";
+		"fmt")
 
 
 func TestRace(t *testing.T){
 
-	slowServer := delayedServer(100)
+	slowServer :=  delayedServer(2)
 	fastServer := delayedServer(0)
+	defer slowServer.Close()
+    defer fastServer.Close()
 
 
     slowURL := slowServer.URL
     fastURL := fastServer.URL
 
     want := fastURL
-    got := race(slowURL, fastURL)
+    got,_ := race(slowURL, fastURL)
 
     if got != want {
         t.Errorf("got '%s', want '%s'", got, want)
     }
+}
 
-    slowServer.Close()
-    fastServer.Close()
+func TestTimeout(t *testing.T) {
+	slowServer := delayedServer(2)
+	fastServer := delayedServer(3)
+	defer slowServer.Close()
+    defer fastServer.Close()
+
+    slowURL := slowServer.URL
+    fastURL := fastServer.URL
+    testtime := time.Second 
+  
+ 	_,got := customrace(slowURL, fastURL, testtime)    
+ 	want := httpGetTimeOut 
+
+    if got!=want {
+        t.Errorf("Error expected, got nil")
+    } else {
+    	fmt.Printf("PASS::%v", httpGetTimeOut.Error())
+    }
+
 }
 
 func delayedServer(t time.Duration) *httptest.Server {
 	 return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        time.Sleep(t * time.Millisecond)
+        time.Sleep(t * time.Second)
         w.WriteHeader(http.StatusOK)
     }))
 }
